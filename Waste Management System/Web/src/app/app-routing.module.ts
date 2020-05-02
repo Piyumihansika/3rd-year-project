bb2045a3d889e386ca7a0ca27040259f04ef012e
MadurangiChathurika <43290660+MadurangiChathurika@users.noreply.github.com>
2020-05-02 19:33:34 +0530

    froent-end
    add login and registration with template

---
+++ Waste Management System/Web/src/app/app-routing.module.ts
@@ -0,0 +1,64 @@
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+
+import {BaseLayoutComponent} from './Layout/base-layout/base-layout.component';
+import {PagesLayoutComponent} from './Layout/pages-layout/pages-layout.component';
+
+
+
+// Dashboards
+
+import {AnalyticsComponent} from './DemoPages/Dashboards/analytics/analutics.component';
+
+// Pages
+
+import {ForgotPasswordBoxedComponent} from './DemoPages/UserPages/forgot-password-boxed/forgot-password-boxed.component';
+import {LoginBoxedComponent} from './DemoPages/UserPages/login-boxed/login-boxed.component';
+import {RegisterBoxedComponent} from './DemoPages/UserPages/register-boxed/register-boxed.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: BaseLayoutComponent,
+    children: [
+
+      // Dashboads
+
+      {path: '', component: AnalyticsComponent, data: {extraParameter: 'dashboardsMenu'}},
+    ]
+
+  },
+  {
+    path: '',
+    component: PagesLayoutComponent,
+    children: [
+
+      // User Pages
+
+      {path: 'pages/login-boxed', component: LoginBoxedComponent, data: {extraParameter: ''}},
+      {path: 'pages/register-boxed', component: RegisterBoxedComponent, data: {extraParameter: ''}},
+      {path: 'pages/forgot-password-boxed', component: ForgotPasswordBoxedComponent, data: {extraParameter: ''}},
+    ]
+  },
+  {path: '**', redirectTo: ''}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes,
+    {
+      scrollPositionRestoration: 'enabled',
+      anchorScrolling: 'enabled',
+    })],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {
+}
+
+
+
+
+
+
+
+
+
--- Waste Management System/Web/src/app/app.module.ts
+++ Waste Management System/Web/src/app/app.module.ts
@@ -1,35 +1,168 @@
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { RouterModule } from '@angular/router';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { ToastrModule } from 'ngx-toastr';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {NgReduxModule} from '@angular-redux/store';
+import {NgRedux, DevToolsExtension} from '@angular-redux/store';
+import {rootReducer, ArchitectUIState} from './ThemeOptions/store';
+import {ConfigActions} from './ThemeOptions/store/config.actions';
+import {AppRoutingModule} from './app-routing.module';
+import {LoadingBarRouterModule} from '@ngx-loading-bar/router';
 
-import { AppRoutingModule } from './app.routing';
-import { ComponentsModule } from './components/components.module';
+import {CommonModule} from '@angular/common';
+import {HttpClientModule} from '@angular/common/http';
+import {AppComponent} from './app.component';
 
-import { AppComponent } from './app.component';
+// BOOTSTRAP COMPONENTS
 
-import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
+import {AngularFontAwesomeModule} from 'angular-font-awesome';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {PerfectScrollbarModule} from 'ngx-perfect-scrollbar';
+import {PERFECT_SCROLLBAR_CONFIG} from 'ngx-perfect-scrollbar';
+import {PerfectScrollbarConfigInterface} from 'ngx-perfect-scrollbar';
+import {ChartsModule} from 'ng2-charts';
+
+// LAYOUT
+
+import {BaseLayoutComponent} from './Layout/base-layout/base-layout.component';
+import {PagesLayoutComponent} from './Layout/pages-layout/pages-layout.component';
+import {PageTitleComponent} from './Layout/Components/page-title/page-title.component';
+
+// HEADER
+
+import {HeaderComponent} from './Layout/Components/header/header.component';
+import {SearchBoxComponent} from './Layout/Components/header/elements/search-box/search-box.component';
+import {UserBoxComponent} from './Layout/Components/header/elements/user-box/user-box.component';
+
+// SIDEBAR
+
+import {SidebarComponent} from './Layout/Components/sidebar/sidebar.component';
+import {LogoComponent} from './Layout/Components/sidebar/elements/logo/logo.component';
+
+// FOOTER
+
+import {FooterComponent} from './Layout/Components/footer/footer.component';
+
+// DEMO PAGES
+
+// Dashboards
+
+import {AnalyticsComponent} from './DemoPages/Dashboards/analytics/analutics.component';
+
+// Pages
+
+import {ForgotPasswordBoxedComponent} from './DemoPages/UserPages/forgot-password-boxed/forgot-password-boxed.component';
+import {LoginBoxedComponent} from './DemoPages/UserPages/login-boxed/login-boxed.component';
+import {RegisterBoxedComponent} from './DemoPages/UserPages/register-boxed/register-boxed.component';
+
+
+
+
+
+
+
+
+
+
+
+
+const DEFAULT_PERFECT_SCROLLBAR_CONFIG: PerfectScrollbarConfigInterface = {
+  suppressScrollX: true
+};
 
 @NgModule({
-  imports: [
-    BrowserAnimationsModule,
-    FormsModule,
-    HttpClientModule,
-    ComponentsModule,
-    RouterModule,
-    AppRoutingModule,
-    NgbModule,
-    ToastrModule.forRoot()
-  ],
   declarations: [
-    AppComponent,
-    AdminLayoutComponent
 
+    // LAYOUT
+
+    AppComponent,
+    BaseLayoutComponent,
+    PagesLayoutComponent,
+    PageTitleComponent,
+
+    // HEADER
+
+    HeaderComponent,
+    SearchBoxComponent,
+    UserBoxComponent,
+
+    // SIDEBAR
+
+    SidebarComponent,
+    LogoComponent,
+
+    // FOOTER
+
+    FooterComponent,
+
+    // DEMO PAGES
+
+    // Dashboards
+
+    AnalyticsComponent,
+
+    // User Pages
+
+    ForgotPasswordBoxedComponent,
+    LoginBoxedComponent,
+    RegisterBoxedComponent,
+
+   
+
+    
+
+    
+
+    
+    
+
+    
   ],
-  providers: [],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    NgReduxModule,
+    CommonModule,
+    LoadingBarRouterModule,
+
+    // Angular Bootstrap Components
+
+    PerfectScrollbarModule,
+    NgbModule,
+    AngularFontAwesomeModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+
+    // Charts
+
+    ChartsModule,
+  ],
+  providers: [
+    {
+      provide:
+      PERFECT_SCROLLBAR_CONFIG,
+      // DROPZONE_CONFIG,
+      useValue:
+      DEFAULT_PERFECT_SCROLLBAR_CONFIG,
+      // DEFAULT_DROPZONE_CONFIG,
+    },
+    ConfigActions,
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+
+export class AppModule {
+  constructor(private ngRedux: NgRedux<ArchitectUIState>,
+              private devTool: DevToolsExtension) {
+
+    this.ngRedux.configureStore(
+      rootReducer,
+      {} as ArchitectUIState,
+      [],
+      [devTool.isEnabled() ? devTool.enhancer() : f => f]
+    );
+
+  }
+}
