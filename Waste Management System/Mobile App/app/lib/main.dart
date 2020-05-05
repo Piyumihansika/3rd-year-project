b5356b88c40cbeb2358e39b480799581603810ff
piyumi Hansika <piyumihansi02@gmail.com>
2020-05-04 21:08:57 -0800

    Add Device Preview

--- Waste Management System/Mobile App/app/lib/main.dart
+++ Waste Management System/Mobile App/app/lib/main.dart
@@ -8,20 +8,27 @@ import 'package:app/GiveToUs/Profile.dart';
 import 'package:app/GiveToUs/Scheduled.dart';
 import 'package:app/GiveToUs/SpecialPick.dart';
 import 'package:flutter/material.dart';
-
+import 'package:device_preview/device_preview.dart';
 
 
 
 
 void main() {
-  runApp(MyApp());
+  
+runApp(
+  DevicePreview(
+    builder: (context) => MyApp(),
+  ),
+);
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Count Increasing App',
+      locale: DevicePreview.of(context).locale, // <--- Add the locale
+      builder: DevicePreview.appBuilder,
+      title: 'Waste Management App',
       home:Login() ,
 
 //To write routes names
