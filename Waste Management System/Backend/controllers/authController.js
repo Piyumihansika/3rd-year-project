ae9b2383c3214bd7d106449a3a48f8790038c8cd
sachiAy <sachiniayeshika943@gmail.com>
2020-05-07 14:04:04 +0530

    login and register(2020/05/07)
    create the authcontroller page that contain the login, registration and logout.

---
+++ Waste Management System/Backend/controllers/authController.js
@@ -0,0 +1,63 @@
+const {Router} = require('express');
+const router = Router();
+
+const User = require('../model/userModel');
+const verifyToken = require('./verifyToken');
+
+const jwt = require('jsonwebtoken');
+const config = require('../config');
+
+router.post('/customerSignup', async(req, res) => {
+    try{
+        const {firstName,lastName,email,username,address,contactNumber,password} = req.body;
+
+        const user = new User({
+            firstName,
+            lastName,
+            email,
+            username,
+            address,
+            contactNumber,
+            password
+        });
+        user.password = await user.encryptPassword(password);
+        await user.save();
+
+        const token = jwt.sign({ id: user.id}, config.secret, {
+            expiresIn: "24h"
+        });
+        res.status(200).json({ auth: true, token})
+
+    }catch(e){
+        console.log(e);
+        res.status(500).send('There was a problem signin');
+
+    }
+});
+
+router.post('/customerSignin', async(req, res) => {
+    try{
+        const user = await User.findOne({ username: req.body.username })
+        if(!user){
+            return res.status(404).send("The username doesn't exist")
+        }
+        const validPassword = await user.validatePassword(req.body.password, user.password);
+        if(!validPassword){
+            return res.status(401).send({ auth: false, token: null});
+        }
+        const token = jwt.sign({ id: user._id }, config.secret, {
+            expiresIn: '24h'
+        });
+        res.status(200).json({ auth: true, token });
+    }catch(e){
+        console.log(e);
+        res.status(500).send("There was a problem signin");
+
+    }
+});
+
+router.get('/logout', function(req, res) {
+    res.status(200).send({ auth: false, token: null })
+});
+
+module.exports = router;
--- Waste Management System/Backend/index.js
+++ Waste Management System/Backend/index.js
@@ -1,37 +1,9 @@
-const connection = require("./model");
+const app = require('./app');
+require('./connection');
 
-const express = require("express");
-const application = express();
-const path = require("path");
-const expressHandlebars = require("express-handlebars");
-const bodyparser = require("body-parser");
+async function init() {
+    await app.listen(3000);
+    console.log('server on localhost:3000')
+}
 
-const UserController = require("./controllers/users");
-
-application.use(bodyparser.json());
-application.use(bodyparser.urlencoded({
-    extended : true
-}));
-
-// application.set('views', path.join(__dirname, "/views/"));
-
-// application.engine("hbs", expressHandlebars({
-//     extname : "hbs",
-//     defaultLayout: "mainlayout",
-//     layoutsDir : __dirname + "/views/layouts"
-// }));
-
-// application.set("view engine", "hbs");
-
-// application.get("/", (req, res)=> {
-//     // res.send('<h1>Hello World</h1>')
-//     res.render("index", {}) 
-// })
-
-application.use("/user", UserController)
-
-application.listen("3000", ()=>{
-    console.log("Server started");
-})
-
- 
+init();
