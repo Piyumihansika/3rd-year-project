44258fd8d05dac78036805d070ceb89809d797b6
Madurangi Chathurika <chathurikamadu96@gmail.com>
2020-06-22 12:45:36 +0530

    update

--- Waste Management System/Buyer App/buyer_app/lib/Models/loginmodel.dart
+++ Waste Management System/Buyer App/buyer_app/lib/Models/loginmodel.dart
@@ -1,44 +1,30 @@
 import 'dart:convert';
 
-
-
 LoginModel loginModelFromJson(String str)=> LoginModel.fromJson(json.decode(str));
-
 String loginModelToJson(LoginModel data)=> json.encode(data.toJson());
 
 class LoginModel {
 
+	String email;
+	String password;
 
-  String email;
+    LoginModel({
 
-  String password;
-
-  LoginModel({
-
-
-    this.email,
+	this.email,
     this.password,
 
-
-  });
+});
 
   factory LoginModel.fromJson(Map<String, dynamic>json)=> LoginModel(
 
     email: json["email"],
-
-    password: json["password"],
-
-
+	password: json["password"],
   );
+  
   Map<String, dynamic>toJson() =>{
 
     "email" :email,
+	"password" :password,
 
-    "password" :password,
-
-
-
-  };
-
-
+ };
 }
