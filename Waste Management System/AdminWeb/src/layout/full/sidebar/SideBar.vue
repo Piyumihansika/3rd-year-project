63eb73686f1fa41aab38809b9efc270fb25c958f
Chamindu Udesh <udeshchamindu@gmail.com>
2020-07-05 23:32:22 +0530

    Design Buyer page

--- Waste Management System/AdminWeb/src/layout/full/sidebar/SideBar.vue
+++ Waste Management System/AdminWeb/src/layout/full/sidebar/SideBar.vue
@@ -4,8 +4,8 @@
     <vs-sidebar default-index="1" :parent="parent" :hiddenBackground="doNotClose" color="primary" class="sidebarx" spacer v-model="isSidebarActive" :click-not-close="doNotClose" >
         <div class="header-sidebar text-center" slot="header">
             <vs-avatar size="70px" src="https://randomuser.me/api/portraits/men/85.jpg"/>
-            <h4>Steave Jobs<br/>
-              <small>varun@gmail.com</small>
+            <h4>Admin<br/>
+              <small>admin@gmail.com</small>
             </h4>
         </div>
         <template v-for="(sidebarLink, index) in sidebarLinks" >
