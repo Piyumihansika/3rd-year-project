ca9c82e008b399accd0d6d9d5369345302870d8b
Chamindu Udesh <udeshchamindu@gmail.com>
2020-07-06 09:31:41 +0530

    Add features to Setting page

--- Waste Management System/AdminWeb/src/views/components/dashboard/settings.vue
+++ Waste Management System/AdminWeb/src/views/components/dashboard/settings.vue
@@ -21,6 +21,49 @@
           </vs-card>
           </vs-col>
             <vs-divider/>
+
+                      <vs-col type="flex" vs-justify="right" vs-align="right" vs-w="6" vs-sm="12">
+          <vs-card>
+          <div slot="header">
+          <h3>
+          Customer Accounts
+          </h3>
+          </div>
+          <div>
+                You can add, view , delete customer here
+          </div>
+          <div slot="footer">
+          <vs-row vs-justify="flex-end">
+          <vs-button class="mr-2" type="gradient" color="danger" icon="delete"></vs-button>
+          <vs-button class="mr-2" color="primary" icon="add"></vs-button>
+          <vs-button color="#18cd5b" color-text="rgb(50,50,50)" icon="person"></vs-button>
+          </vs-row>
+          </div>
+          </vs-card>
+          </vs-col>
+            <vs-divider/>
+
+
+                      <vs-col type="flex" vs-justify="right" vs-align="right" vs-w="6" vs-sm="12">
+          <vs-card>
+          <div slot="header">
+          <h3>
+          Buyer Accounts
+          </h3>
+          </div>
+          <div>
+                You can add, view, delete buyer here
+          </div>
+          <div slot="footer">
+          <vs-row vs-justify="flex-end">
+          <vs-button class="mr-2" type="gradient" color="danger" icon="delete"></vs-button>
+          <vs-button class="mr-2" color="primary" icon="add"></vs-button>
+          <vs-button color="#18cd5b" color-text="rgb(50,50,50)" icon="person"></vs-button>
+          </vs-row>
+          </div>
+          </vs-card>
+          </vs-col>
+            <vs-divider/>
           </vs-row>
 </template>
 <script>
