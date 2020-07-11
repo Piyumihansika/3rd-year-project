0ac5f058cfc09eda48693ff95d54e45cf52bc0b0
piyumi Hansika <piyumihansi02@gmail.com>
2020-07-11 13:58:13 -0800

    Update Sell_ItemPage2.dart

--- Waste Management System/Mobile App/app/lib/Seller/Sell_Items/Sell_ItemPage2.dart
+++ Waste Management System/Mobile App/app/lib/Seller/Sell_Items/Sell_ItemPage2.dart
@@ -8,8 +8,8 @@ class SellItemPage2 extends StatefulWidget {
 class _SellItemPage2State extends State<SellItemPage2> {
     final _formKey = GlobalKey<FormState>();
 
-    TextEditingController description = TextEditingController();
-  TextEditingController price = TextEditingController();
+  // TextEditingController description = TextEditingController();
+  // TextEditingController price = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -19,7 +19,7 @@ class _SellItemPage2State extends State<SellItemPage2> {
     icon: Icon(Icons.arrow_back, color: Colors.black),
     onPressed: () => Navigator.of(context).pushNamed('/dashboard'),
   ), 
-          title: Text('Add Items To Bid'),
+          title: Text('Add Item Images'),
           backgroundColor: Colors.green,  
         ),  
         body: Form(
@@ -31,53 +31,6 @@ mainAxisAlignment: MainAxisAlignment.center,
         
         children: <Widget>[
 
-   Container(
-              padding: EdgeInsets.all(10),
-              child: TextFormField(
-                controller: price,
-                scrollPadding: EdgeInsets.all(10),
-                decoration: const InputDecoration(
-                  
-                  hintText: 'Enter a base price',
-                  labelText: 'Base Price',
-                  labelStyle: TextStyle(
-                      fontWeight: FontWeight.bold, color: Colors.green),
-                ),
-
-                
-                validator: (value) {
-                  if (value.isEmpty) {
-                    return 'Please enter some price';
-                  }
-                  return null;
-                },
-              ),
-            ),
-
-Container(
-  child: Padding(
-    padding: const EdgeInsets.all(15.0),
-    child: TextField(
-      minLines: 8,
-      maxLines: 12,
-      autocorrect: false,
-      controller: description,
-      decoration: InputDecoration(
-        hintText: 'Write your discription here',
-        filled: true,
-        fillColor: Colors.green[100],
-        enabledBorder: OutlineInputBorder(
-          borderRadius: BorderRadius.all(Radius.circular(10.0)),
-          borderSide: BorderSide(color: Colors.grey),
-        ),
-        focusedBorder: OutlineInputBorder(
-          borderRadius: BorderRadius.all(Radius.circular(10.0)),
-          borderSide: BorderSide(color: Colors.grey),
-        ),
-      ),
-    ),
-  ),
-),
 
 
 
@@ -93,14 +46,11 @@ alignment: Alignment(-0.1,1),
                 onPressed: () {  
                  
 
-                  // print(description.text);
-                  // print(_selectedItem.name);
-                 // print(selectDuration);
+                 
                     //Navigator.of(context).pushNamed('/dashboard1');
                   if (_formKey.currentState.validate()) {  
                  
-               // print(selectDuration);
-                
+        
                    
                   }  
                 },  
