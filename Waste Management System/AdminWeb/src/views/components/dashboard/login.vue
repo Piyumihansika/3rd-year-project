628177753ac9bf5a5635b19046d95ba575db5b9e
Chamindu Udesh <udeshchamindu@gmail.com>
2020-07-15 00:31:21 +0530

    update login

--- Waste Management System/AdminWeb/src/views/components/dashboard/login.vue
+++ Waste Management System/AdminWeb/src/views/components/dashboard/login.vue
@@ -1,5 +1,41 @@
 <template>
-       <div id= "login">
-        <h1>gef Page</h1>
+       <div id= "login" class="container" align = "center">
+           <h1>Admin Login Page</h1>
+           <br>
+           <br>
+           <br>
+
+        <div class="overlay-container">
+            <div class="overlay" >
+               <div align = "left"> <h3> Enter your email </h3>
+                
+                <vs-input icon="mail" placeholder="Email" align="left" class="mt-0" v-model="inputicon1"/>
+               <br>
+               
+                <h3> Enter your password </h3>
+                <vs-input icon="lock" placeholder="Password" align ="left" class="mt-0" v-model="inputicon1"/>
+               </div>
+            </div>
+        </div>
+
+
+
+
+
+
+
+
+
+
+
+
+        
+
+
+
+
+
+
+
     </div>
 </template>
