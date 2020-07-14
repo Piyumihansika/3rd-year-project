4d8bed4576e341782cecc16db3b9e9ee3ca8790f
Chamindu Udesh <udeshchamindu@gmail.com>
2020-07-15 00:52:48 +0530

    change login vue

--- Waste Management System/AdminWeb/src/views/components/dashboard/login.vue
+++ Waste Management System/AdminWeb/src/views/components/dashboard/login.vue
@@ -1,41 +1,45 @@
-<template>
-       <div id= "login" class="container" align = "center">
-           <h1>Admin Login Page</h1>
-           <br>
-           <br>
-           <br>
+  <template lang="html">
+            <div class="centerx">
+            <vs-button @click="popupActivo2=true" color="primary" type="filled"> Login</vs-button>
+            <vs-popup classContent="popup-example" title="Admin Login Page" :active.sync="popupActivo2">
+                <p>
+            Enter your email
+            </p>
+            <vs-input class="inputx" placeholder="Email" v-model="value1"/>
+            <br>
+            <p>
+            Enter your password
+            </p>
 
-        <div class="overlay-container">
-            <div class="overlay" >
-               <div align = "left"> <h3> Enter your email </h3>
-                
-                <vs-input icon="mail" placeholder="Email" align="left" class="mt-0" v-model="inputicon1"/>
-               <br>
-               
-                <h3> Enter your password </h3>
-                <vs-input icon="lock" placeholder="Password" align ="left" class="mt-0" v-model="inputicon1"/>
-               </div>
+            <vs-input class="inputx" placeholder="Password" v-model="value2"/>
+            <br>
+            
+            <!-- <vs-select
+            label="Figuras" v-model="select1"> -->
+            <vs-select-item :key="index" :value="item.value" :text="item.text" v-for="item,index in options1" />
+            </vs-select>
+            <vs-button @click="popupActivo3=true" color="primary" type="filled">Log In</vs-button>
+            
+            </vs-popup>
             </div>
-        </div>
-
-
-
-
-
-
-
-
-
-
-
-
-        
-
-
-
-
-
-
-
-    </div>
 </template>
+
+<script>
+export default {
+     data(){
+            return {
+            select1:3,
+            options1:[
+            {text:'IT',value:0},
+            {text:'Blade Runner',value:2},
+            {text:'Thor Ragnarok',value:3},
+            ],
+            value1:'',
+            value2:'',
+            popupActivo2:false,
+            popupActivo3:false
+            }
+            }
+    
+}
+</script>
