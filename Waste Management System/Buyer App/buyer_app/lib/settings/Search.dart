e9e34f106d4a5a43ff9307a47c141589bbb7daec
Madurangi Chathurika <chathurikamadu96@gmail.com>
2020-07-15 16:54:06 +0530

    location permission

--- Waste Management System/Buyer App/buyer_app/lib/settings/Search.dart
+++ Waste Management System/Buyer App/buyer_app/lib/settings/Search.dart
@@ -1,122 +1,155 @@
-import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
-
-//void main() => runApp(App());
-
-class Search extends StatefulWidget {
-  @override 
-  _AppState createState() => _AppState(); 
-}
-
-class _AppState extends State<Search> {
-
-  String _locationMessage = ""; 
-
-  void _getCurrentLocation() async {
-
-    final position = await Geolocator().getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
-    print(position);
-
-    setState(() {
-      _locationMessage = "${position.latitude}, ${position.longitude}";
-    });
-
-  }
 
 
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      home: Scaffold(
-       appBar: AppBar(
-          title: Text("Location Services")
-        ),
-        body: Align(
-            child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-            Text(_locationMessage), 
-            FlatButton(
-              onPressed: () {
-                  _getCurrentLocation();
-              },
-              color: Colors.green,
-              child: Text("Find Location")
-            )
-          ]),
-        )
-      )
-    );
-  }
-}
+// import 'dart:async';
 
 // import 'package:flutter/material.dart';
-// //import 'package:flutter/services.dart';
+// import 'package:geocoder/geocoder.dart';
 // import 'package:geolocator/geolocator.dart';
 
-// class Search extends StatefulWidget {
+// class Tracking extends StatefulWidget {
+  
+
 //   @override
-//   _HomePageState createState() => _HomePageState();
+//   _TrackingState createState() => _TrackingState();
 // }
 
-// class _HomePageState extends State<Search> {
-//   final Geolocator geolocator = Geolocator()..forceAndroidLocationManager;
+// class _TrackingState extends State<Tracking> {
+//  Position _position;
+//  StreamSubscription<Position> _streamSubscription;
+//  Address _address;
 
-//   Position _currentPosition;
-//   String _currentAddress;
+//  @override
+//  void initState(){
+//    super.initState();
+//  var locationOptions = LocationOptions(accuracy: LocationAccuracy.high, distanceFilter: 10);
+//  _streamSubscription = Geolocator().getPositionStream(locationOptions).listen((Position position) {
 
+//    setState(() {
+//      print(position);
+//      _position = position;
+//      final coordinates = new Coordinates(position.latitude, position.longitude);
+//      convertCoordinatesToAddress(coordinates).then((value) => _address = value);
+//    });
+//   });
+// }
 //   @override
 //   Widget build(BuildContext context) {
 //     return Scaffold(
 //       appBar: AppBar(
-//         title: Text("Location"),
+//         title: Text('Location Tracking'),
+//          backgroundColor: Colors.green,
 //       ),
 //       body: Center(
 //         child: Column(
 //           mainAxisAlignment: MainAxisAlignment.center,
 //           children: <Widget>[
-//             if (_currentPosition != null) Text(_currentAddress),
-//             FlatButton(
-//               child: Text("Get location"),
-//               onPressed: () {
-//                 _getCurrentLocation();
-//               },
-//             ),
-//           ],
-//         ),
-//       ),
+//             Text("lat: ${_position?. latitude?? '-'}, lon: ${_position?. longitude?? '-'}"),
+//             SizedBox(height: 50,),
+//             Text("Address from coordinates"),
+//             SizedBox(height: 20,),
+//             Text("${_address?.addressLine?? '-'}"),
+//           ],),),
 //     );
 //   }
 
-//   _getCurrentLocation() {
-//     geolocator
-//         .getCurrentPosition(desiredAccuracy: LocationAccuracy.best)
-//         .then((Position position) {
-//       setState(() {
-//         _currentPosition = position;
-//       });
+//    @override
+//    void dispose(){
+//      super.dispose();
+//      _streamSubscription.cancel();
+//    }
 
-//       _getAddressFromLatLng();
-//     }).catchError((e) {
-//       print(e);
-//     });
-//   }
-
-//   _getAddressFromLatLng() async {
-//     try {
-//       List<Placemark> p = await geolocator.placemarkFromCoordinates(
-//           _currentPosition.latitude, _currentPosition.longitude);
-
-//       Placemark place = p[0];
-
-//       setState(() {
-//         _currentAddress =
-//             "${place.locality}, ${place.postalCode}, ${place.country}";
-//       });
-//     } catch (e) {
-//       print(e);
-//     }
-//   }
+//    Future<Address> convertCoordinatesToAddress(Coordinates coordinates) async {
+//      var addresses = await Geocoder.local.findAddressesFromCoordinates(coordinates);
+//      return addresses.first;
+//    }
 // }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // import 'dart:async';
+
+// // import 'package:flutter/material.dart';
+// // import 'package:geocoder/geocoder.dart';
+// // import 'package:geolocator/geolocator.dart';
+
+// // class ConvertGeocodeToAddress extends StatefulWidget {
+  
+
+// //   @override
+// //   _ConvertGeocodeToAddressState createState() => _ConvertGeocodeToAddressState();
+// // }
+
+// // class _ConvertGeocodeToAddressState extends State<ConvertGeocodeToAddress> {
+// //  Position _position;
+// //  StreamSubscription<Position> _streamSubscription;
+// //  Address _address;
+
+// //  @override
+// //  void initState(){
+// //    super.initState();
+// //  var locationOptions = LocationOptions(accuracy: LocationAccuracy.high, distanceFilter: 10);
+// //  _streamSubscription = Geolocator().getPositionStream(locationOptions).listen((Position position) {
+
+// //    setState(() {
+// //      print(position);
+// //      _position = position;
+// //      final coordinates = new Coordinates(position.latitude, position.longitude);
+// //      convertCoordinatesToAddress(coordinates).then((value) => _address = value);
+// //    });
+// //   });
+// // }
+// //   @override
+// //   Widget build(BuildContext context) {
+// //     return Scaffold(
+// //       appBar: AppBar(
+// //         title: Text('Location Tracking'),
+// //          backgroundColor: Colors.green,
+// //       ),
+// //       body: Center(
+// //         child: Column(
+// //           mainAxisAlignment: MainAxisAlignment.center,
+// //           children: <Widget>[
+// //             Text("lat: ${_position?. latitude?? '-'}, lon: ${_position?. longitude?? '-'}"),
+// //             SizedBox(height: 50,),
+// //             Text("Address from coordinates"),
+// //             SizedBox(height: 20,),
+// //             Text("${_address?.addressLine?? '-'}"),
+// //           ],),),
+// //     );
+// //   }
+
+// //    @override
+// //    void dispose(){
+// //      super.dispose();
+// //      _streamSubscription.cancel();
+// //    }
+
+// //    Future<Address> convertCoordinatesToAddress(Coordinates coordinates) async {
+// //      var addresses = await Geocoder.local.findAddressesFromCoordinates(coordinates);
+// //      return addresses.first;
+// //    }
+// // }
+
+
