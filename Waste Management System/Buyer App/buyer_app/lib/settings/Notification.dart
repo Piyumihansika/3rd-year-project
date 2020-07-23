e1a17d70b123cc327fc4cf553ff1c769f0ea97c1
Madurangi Chathurika <chathurikamadu96@gmail.com>
2020-07-24 01:58:09 +0530

    start notification function

--- Waste Management System/Buyer App/buyer_app/lib/settings/Notification.dart
+++ Waste Management System/Buyer App/buyer_app/lib/settings/Notification.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 class Notifications extends StatefulWidget {
   @override
@@ -6,6 +8,24 @@ class Notifications extends StatefulWidget {
 }
 
 class _NotificationsState extends State<Notifications> {
+
+  FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin;
+
+  @override
+  initState() {
+    super.initState();
+    // initialise the plugin. app_icon needs to be a added as a drawable resource to the Android head project   
+     // If you have skipped STEP 3 then change app_icon to @mipmap/ic_launcher
+    var initializationSettingsAndroid =
+        new AndroidInitializationSettings('notifi'); 
+    var initializationSettingsIOS = new IOSInitializationSettings();
+    var initializationSettings = new InitializationSettings(
+        initializationSettingsAndroid, initializationSettingsIOS);
+    flutterLocalNotificationsPlugin = new FlutterLocalNotificationsPlugin();
+    flutterLocalNotificationsPlugin.initialize(initializationSettings,
+        onSelectNotification: onSelectNotification);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,6 +34,98 @@ class _NotificationsState extends State<Notifications> {
         backgroundColor: Colors.green,
       ),
       backgroundColor: Colors.white,
+
+      body: new Center(
+          child: new Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisSize: MainAxisSize.max,
+            children: <Widget>[
+              new RaisedButton(
+                onPressed: _showNotificationCancelBid,
+                child: new Text('Show Notification With Sound'),
+              ),
+            ]
+          )
+      )
     );
   }
+
+  Future onSelectNotification(String currentBid) async {
+    showDialog(
+      context: context,
+      builder: (_) {
+        return new AlertDialog(
+          title: Text("Current Bid"),
+          content: Text("Current Bid : $currentBid"),
+        );
+      },
+    );
+  }
+
+  // Future onWinNotification(String currentBid) async {
+  //   showDialog(
+  //     context: context,
+  //     builder: (_) {
+  //       return new AlertDialog(
+  //         title: Text("Congradulations!"),
+  //         content: Text("Your Bid : $currentBid" ),
+  //       );
+  //     },
+  //   );
+  // }
+
+
+// Future _showNotificationWinner() async {
+//   var androidPlatformChannelSpecifics = new AndroidNotificationDetails(
+//       'your channel id', 'your channel name', 'your channel description',
+//       sound: 'swiftly',
+//       importance: Importance.Max,
+//       priority: Priority.High);
+//   var iOSPlatformChannelSpecifics =
+//       new IOSNotificationDetails(sound: "swiftly");
+//   var platformChannelSpecifics = new NotificationDetails(
+//       androidPlatformChannelSpecifics, iOSPlatformChannelSpecifics);
+//   await flutterLocalNotificationsPlugin.show(
+//     0,
+//     'Winner',
+//     'You win the bid',
+//     platformChannelSpecifics,
+//     payload: '5000',
+//   );
+// }
+
+// Method 2
+Future _showNotificationCancelBid() async {
+  var androidPlatformChannelSpecifics = new AndroidNotificationDetails(
+      'your channel id', 'your channel name', 'your channel description',
+      importance: Importance.Max, priority: Priority.High);
+  var iOSPlatformChannelSpecifics = new IOSNotificationDetails();
+  var platformChannelSpecifics = new NotificationDetails(
+      androidPlatformChannelSpecifics, iOSPlatformChannelSpecifics);
+  await flutterLocalNotificationsPlugin.show(
+    0,
+    'New Alert',
+    'Your bid is canceled!',
+    platformChannelSpecifics,
+    payload: '200',
+  );
+}
+
+// // Method 3
+// Future _showNotificationWithoutSound() async {
+//   var androidPlatformChannelSpecifics = new AndroidNotificationDetails(
+//       'your channel id', 'your channel name', 'your channel description',
+//       playSound: false, importance: Importance.Max, priority: Priority.High);
+//   var iOSPlatformChannelSpecifics =
+//       new IOSNotificationDetails(presentSound: false);
+//   var platformChannelSpecifics = new NotificationDetails(
+//       androidPlatformChannelSpecifics, iOSPlatformChannelSpecifics);
+//   await flutterLocalNotificationsPlugin.show(
+//     0,
+//     'New Post',
+//     'How to Show Notification in Flutter',
+//     platformChannelSpecifics,
+//     payload: 'No_Sound',
+//   );
+// }
 }
