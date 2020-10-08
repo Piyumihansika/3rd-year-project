859143c8a902995cd7758a670a6b20349fd56762
piyumi Hansika <piyumihansi02@gmail.com>
2020-10-07 18:43:28 -0700

    notification module
    version 1

--- Waste Management System/Mobile App/app/lib/Seller/History.dart
+++ Waste Management System/Mobile App/app/lib/Seller/History.dart
@@ -1,56 +1,41 @@
 import 'package:flutter/material.dart';
-//import 'package:badges/badges.dart';
+
+
+class History extends StatefulWidget {
+  createState() => HistoryState();
+}
 
 List<String> listItems = [
   "Hello",
   "World",
   "Flutter",
   "Love"
-]; 
+]; //dummy list of items
 
-
-class History extends StatefulWidget {
-  @override
-  _HistoryState createState() => _HistoryState();
-}
-
-class _HistoryState extends State<History> {
-  
+class HistoryState extends State<History> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
- appBar: AppBar(
-   leading: IconButton(
-    icon: Icon(Icons.arrow_back, color: Colors.black),
-    onPressed: () => Navigator.of(context).pushNamed('/dashboard'),
-  ), 
-        title: Text("NOTIFICATION HISTORY",
-      style: TextStyle(color: Colors.black),),
-        backgroundColor: Colors.green,
-      ),
-      backgroundColor: Colors.green[100],     
-    
-
-//Here add coding about badges
-
-body : SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-  Padding(padding: const EdgeInsets.all(10.0)),
-
-Container(child: ListView.builder(
-  shrinkWrap: true,
-          itemCount: listItems.length,
-          itemBuilder: (context, index) {
-            return Dismissible(
-              key: ObjectKey(listItems[index]),
-              child: Container(
-                padding: EdgeInsets.all(20.0),
-                child: Text(listItems[index]),
-              ),
-//
-   onDismissed: (direction) {
+    return MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text("Swipe List"),
+          ),
+          body: Container(
+              child: ListView.builder(
+            itemCount: listItems.length,
+            itemBuilder: (context, index) {
+              return Dismissible(
+                background: stackBehindDismiss(),
+                key: ObjectKey(listItems[index]),
+                child: Container(
+                  padding: EdgeInsets.all(20.0),
+                  child: Text(listItems[index]),
+                ),
+                onDismissed: (direction) {
                   var item = listItems.elementAt(index);
                   //To delete
                   deleteItem(index);
@@ -64,66 +49,13 @@ Container(child: ListView.builder(
                             undoDeletion(index, item);
                           })));
                 },
-
-
-            );
-          },
-        )
-        
-        ),
-
-
-            ],
-          ),
-),
-
-
-            
-
-// 
-// body:Column(
-
-// children: <Widget>[
-
-// Container(
-
-//    child: ListView.builder(
-//           itemCount: listItems.length,
-//           itemBuilder: (context, index) {
-//             return Dismissible(
-//               key: ObjectKey(listItems[index]),
-//               child: Container(
-//                 padding: EdgeInsets.all(20.0),
-//                 child: Text(listItems[index]),
-//               ),
-//             );
-//           },
-//         )
-// )
-// Badge(
-  
-//   // child:Icon(Icons.add),
-//   child:RaisedButton(child: Text('notification'),onPressed: (){},),
-//   badgeContent: Text("2"),
-//   badgeColor: Colors.red,
-//   animationType: BadgeAnimationType.slide,
-
-
-// )
-
-// ],
-
-
-// ) ,
-
-
-    );
+              );
+            },
+          )),
+        ));
   }
 
-
-  //
-
-void deleteItem(index) {
+  void deleteItem(index) {
     /*
     By implementing this method, it ensures that upon being dismissed from our widget tree, 
     the item is removed from our list of items and our list is updated, hence
@@ -151,14 +83,200 @@ void deleteItem(index) {
       color: Colors.red,
       child: Icon(
         Icons.delete,
-        color: Colors.red,
+        color: Colors.white,
       ),
     );
   }
-
-
-  //
 }
 
 
+// List<String> listItems = [
+//   "Your Plastic bid is over .Max price is Rs.450",
+//   "World",
+//   "Flutter",
+//   "Love"
+// ]; 
 
+
+// class History extends StatefulWidget {
+//   @override
+//   _HistoryState createState() => _HistoryState();
+// }
+
+// class _HistoryState extends State<History> {
+  
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//  appBar: AppBar(
+//    leading: IconButton(
+//     icon: Icon(Icons.arrow_back, color: Colors.black),
+//     onPressed: () => Navigator.of(context).pushNamed('/dashboard'),
+//   ), 
+//         title: Text("NOTIFICATION HISTORY",
+//       style: TextStyle(color: Colors.black),),
+//         backgroundColor: Colors.green,
+//       ),
+//       backgroundColor: Colors.green[100],     
+    
+
+
+// body : SingleChildScrollView(
+//           child: Column(
+//             mainAxisAlignment: MainAxisAlignment.center,
+//             children: <Widget>[
+//   Padding(padding: const EdgeInsets.all(10.0)),
+
+// Container(child: ListView.builder(
+//   shrinkWrap: true,
+//           itemCount: listItems.length,
+//           itemBuilder: (context, index) {
+//             return Dismissible(
+//               key: ObjectKey(listItems[index]),
+//               child: Container(
+//                 padding: EdgeInsets.all(20.0),
+//                 child: Text(listItems[index]),
+//               ),
+// //
+//    onDismissed: (direction) {
+//                   var item = listItems.elementAt(index);
+//                   //
+                  
+//                   //To delete
+//                   deleteItem(index);
+//                   //To show a snackbar with the UNDO button
+//                   Scaffold.of(context).showSnackBar(SnackBar(
+//                       content: Text("Item deleted"),
+//                       action: SnackBarAction(
+//                           label: "UNDO",
+//                           onPressed: () {
+//                             //To undo deletion
+//                             undoDeletion(index, item);
+//                           })));
+//                 },
+
+
+//             );
+//           },
+//         )
+        
+//         ),
+
+
+//             ],
+//           ),
+// ),
+
+
+            
+
+
+
+
+//     );
+//   }
+
+
+//   //
+
+// void deleteItem(index) {
+//     //
+
+
+//     setState(() {
+//       listItems.removeAt(index);
+//     });
+//   }
+
+//   void undoDeletion(index, item) {
+    
+
+//     setState(() {
+//       listItems.insert(index, item);
+//     });
+//   }
+
+//   Widget stackBehindDismiss() {
+//     return Container(
+//       alignment: Alignment.centerRight,
+//       padding: EdgeInsets.only(right: 20.0),
+//       color: Colors.red,
+//       child: Icon(
+//         Icons.delete,
+//         color: Colors.black,
+//       ),
+//     );
+//   }
+
+
+//   //
+// }
+
+
+
+
+// import 'package:flutter/foundation.dart';
+// import 'package:flutter/material.dart';
+
+
+// class History extends StatefulWidget {
+//   History({Key key}) : super(key: key);
+
+//   @override
+//   HistoryState createState() {
+//     return HistoryState();
+//   }
+// }
+
+// class HistoryState extends State<History> {
+//   final items = List<String>.generate(5, (i) => "Item ${i + 1}");
+
+//   @override
+//   Widget build(BuildContext context) {
+//     final title = 'History';
+
+//     return MaterialApp(
+//       title: title,
+//       theme: ThemeData(
+//         primarySwatch: Colors.blue,
+//       ),
+//       home: Scaffold(
+//         appBar: AppBar(
+//    leading: IconButton(
+//     icon: Icon(Icons.arrow_back, color: Colors.black),
+//     onPressed: () => Navigator.of(context).pushNamed('/dashboard'),
+//   ), 
+//         title: Text("NOTIFICATION HISTORY",
+//       style: TextStyle(color: Colors.black),),
+//         backgroundColor: Colors.green,
+//       ),
+//       backgroundColor: Colors.green[100],
+//         body: ListView.builder(
+//           itemCount: items.length,
+//           itemBuilder: (context, index) {
+//             final item = items[index];
+
+//             return Dismissible(
+            
+//               key: Key(item),
+              
+//               onDismissed: (direction) {
+              
+//                 setState(() {
+//                   items.removeAt(index);
+//                 });
+
+              
+//                 Scaffold.of(context)
+//                     .showSnackBar(SnackBar(content: Text("Notification dismissed")));
+//               },
+             
+//               background: Container(color: Colors.red),
+//               child: ListTile(title: Text('Polythin bid is over')),
+//             );
+//           },
+//         ),
+//       ),
+//     );
+//   }
+// }
